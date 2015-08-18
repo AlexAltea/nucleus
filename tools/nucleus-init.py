865d8c34f258aef894060835dbb82cde207ec697
Alexandro Sánchez Bach <alexandro.sanchez@stud.uni-heidelberg.de>
2015-08-19 00:16:26 +0200

    Removed LLVM submodule

--- tools/nucleus-init.py
+++ tools/nucleus-init.py
@@ -4,25 +4,8 @@ import os
 
 # Directories
 NUCLEUS_DIR = os.path.abspath("..")
-NUCLEUS_LLVM = os.path.join(NUCLEUS_DIR, "externals", "llvm")
 
 def init_windows():
-    # Configure LLVM
-    command = "cmake"
-    command += " -G \"Visual Studio 12 2013 Win64\""
-    command += " -DCMAKE_CONFIGURATION_TYPES=\"Debug;Release\""
-    command += " -DLLVM_TARGETS_TO_BUILD=X86"
-    command += " -DLLVM_BUILD_RUNTIME=OFF"
-    command += " -DLLVM_BUILD_TOOLS=OFF"
-    command += " -DLLVM_INCLUDE_DOCS=OFF"
-    command += " -DLLVM_INCLUDE_EXAMPLES=OFF"
-    command += " -DLLVM_INCLUDE_TESTS=OFF"
-    command += " -DLLVM_INCLUDE_TOOLS=OFF"
-    command += " -DLLVM_INCLUDE_UTILS=OFF"
-    command += " -DWITH_POLLY=OFF"
-    command += " ."
-    os.chdir(NUCLEUS_LLVM)
-    os.system(command)
     return
 
 def init_linux():
