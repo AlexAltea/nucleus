554847b8963aedae4cc7ad6127d00c45752839f9
Alexandro Sánchez Bach <alexandro.sanchez@stud.uni-heidelberg.de>
2015-09-02 00:51:30 +0200

    Updated CPU IR Function class

--- nucleus/cpu/hir/function.cpp
+++ nucleus/cpu/hir/function.cpp
@@ -4,11 +4,40 @@
  */
 
 #include "function.h"
+#include "nucleus/cpu/hir/module.h"
 
 namespace cpu {
 namespace hir {
 
-Function::Function(Module* parent) : parent(parent) {
+Function::Function(Module* parent, TypeOut tOut, TypeIn tIn)
+    : parent(parent), typeOut(tOut), typeIn(tIn), flags(0) {
+    // Set flags
+    flags |= FUNCTION_IS_DECLARED;
+
+    // Argument values
+    for (auto type : tIn) {
+        Value* value = new Value();
+        value->type = type;
+        args.push_back(value);
+    }
+
+    // Add function to parent module
+    parent->addFunction(this);
+}
+
+Function::~Function() {
+    for (auto block : blocks) {
+        delete block;
+    }
+}
+
+bool Function::compile() {
+    if (flags & FUNCTION_IS_COMPILED) {
+        return false;
+    }
+
+    flags |= FUNCTION_IS_COMPILED;
+    return true;
 }
 
 }  // namespace hir
--- nucleus/cpu/hir/function.h
+++ nucleus/cpu/hir/function.h
@@ -6,6 +6,10 @@
 #pragma once
 
 #include "nucleus/common.h"
+#include "nucleus/cpu/hir/type.h"
+#include "nucleus/cpu/hir/value.h"
+
+#include <vector>
 
 namespace cpu {
 namespace hir {
@@ -13,16 +17,55 @@ namespace hir {
 class Block;
 class Module;
 
+enum FunctionFlags {
+    FUNCTION_IS_DECLARED   = (1 << 0),  // Function is referenced in its parent module
+    FUNCTION_IS_DEFINING   = (1 << 1),  // Function is being analyzed and IR emitted
+    FUNCTION_IS_DEFINED    = (1 << 1),  // Function has been analyzed and IR emitted
+    FUNCTION_IS_COMPILING  = (1 << 2),  // Function is being compiled
+    FUNCTION_IS_COMPILED   = (1 << 2),  // Function has been compiled
+};
+
 class Function {
-	Module* parent;
+    using TypeOut = Type;
+    using TypeIn = std::vector<Type>;
+
+    Module* parent;
+
+    // Pointer to the compiled function
+    void* emittedFunction;
 
 public:
-	Block* entry;
+    // Function flags
+    U32 flags;
+
+    // Return and argument types
+    TypeOut typeOut;
+    TypeIn typeIn;
+
+    // Entry point
+    Block* entry;
+    std::vector<Block*> blocks;
+
+    std::vector<Value*> args;
 
     // Constructor
-	Function(Module* parent);
+    Function(Module* parent, TypeOut tOut, TypeIn tIn);
+    ~Function();
 
+    template <typename... Ts>
+    U64 call(Ts... args) {
+        return ((U64(*)(Ts...))emittedFunction)(args...);
+    }
     
+    // Frontend methods
+
+    /**
+     * Analyze this function emitting the IR
+     * @return           True on success
+     */
+    bool analyze();
+
+
     // Backend methods
 
     /**
