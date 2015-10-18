87d061e99fec16bcb5bc9cbff4201a6eebdc7bb6
Alexandro Sánchez Bach <alexandro.sanchez@stud.uni-heidelberg.de>
2015-10-18 14:42:24 +0100

    Implemented and* PPU unit tests

--- tests/unit/cpu/test_ppu.cpp
+++ tests/unit/cpu/test_ppu.cpp
@@ -99,7 +99,7 @@ public:
         test_add(0x000000010000FFFFULL, 0x0000000200000001ULL, 0x0000000300010000ULL);
         test_add(0xFFFFFFFFFFFFFFFFULL, 0x0000000000000001ULL, 0x0000000000000000ULL);
 
-        // Add with condition
+        // Add (with condition)
         TEST_INSTRUCTION(test_add_, R1, R2, R3, LT, GT, EQ, SO, {
             state.r[1] = R1;
             state.r[2] = R2;
@@ -141,7 +141,7 @@ public:
         test_addc(0xFFFFFFFFFFFFFFF0ULL, 0x000000000000000FULL, true,  0xFFFFFFFFFFFFFFFFULL, false);
         test_addc(0x000000000000FFFFULL, 0xFFFFFFFFFFFF0011ULL, true,  0x0000000000000010ULL, true);
 
-        // Add Carrying with condition
+        // Add Carrying (with condition)
         TEST_INSTRUCTION(test_addc_, R1, R2, oldCA, R3, newCA, LT, GT, EQ, SO, {
             state.r[1] = R1;
             state.r[2] = R2;
@@ -185,7 +185,7 @@ public:
         test_adde(0x000000000000FFFFULL, true,  0x0000000000000000ULL, 0x0000000000010000ULL, false);
         test_adde(0xFFFFFFFFFFFFFFF0ULL, true,  0x000000000000000FULL, 0x0000000000000000ULL, true);
         
-        // Add Extended with condition
+        // Add Extended (with condition)
         TEST_INSTRUCTION(test_adde_, R1, R2, oldCA, R3, newCA, LT, GT, EQ, SO, {
             state.r[1] = R1;
             state.r[2] = R2;
@@ -222,13 +222,13 @@ public:
             then(!state.xer.ca);
         });
 
-        test_addi(r1, 0x00000001'0000FFFFULL, 0x0001, 0x00000001'00010000ULL);
-        test_addi(r1, 0x00000000'FFFFFFFFULL, 0x0001, 0x00000001'00000000ULL);
-        test_addi(r1, 0x00000000'FFFF0001ULL, 0xFFFF, 0x00000000'FFFF0000ULL);
-        test_addi(r1, 0xFFFFFFFF'FFFFFFFFULL, 0x0001, 0x00000000'00000000ULL);
-        test_addi(r0, 0x00000001'0000FFFFULL, 0x0001, 0x00000000'00000001ULL);
-        test_addi(r0, 0x00000000'FFFFFFFFULL, 0xFFFF, 0xFFFFFFFF'FFFFFFFFULL);
-        test_addi(r0, 0x00000000'FFFF0001ULL, 0x0000, 0x00000000'00000000ULL);
+        test_addi(r1, 0x000000010000FFFFULL, 0x0001, 0x0000000100010000ULL);
+        test_addi(r1, 0x00000000FFFFFFFFULL, 0x0001, 0x0000000100000000ULL);
+        test_addi(r1, 0x00000000FFFF0001ULL, 0xFFFF, 0x00000000FFFF0000ULL);
+        test_addi(r1, 0xFFFFFFFFFFFFFFFFULL, 0x0001, 0x0000000000000000ULL);
+        test_addi(r0, 0x000000010000FFFFULL, 0x0001, 0x0000000000000001ULL);
+        test_addi(r0, 0x00000000FFFFFFFFULL, 0xFFFF, 0xFFFFFFFFFFFFFFFFULL);
+        test_addi(r0, 0x00000000FFFF0001ULL, 0x0000, 0x0000000000000000ULL);
     }
 
     TEST_METHOD(PPC_addic) {
@@ -246,13 +246,129 @@ public:
             then(state.xer.ca == CA);
         });
 
-        test_addic(0x00000001'0000FFFFULL, 0x0001, 0x00000001'00010000ULL, false);
-        // TODO: test_addic(0x00000000'FFFFFFFFULL, 0x0001, 0x00000001'00000000ULL, true);
-        // TODO: test_addic(0x00000000'FFFF0001ULL, 0xFFFF, 0x00000000'FFFF0000ULL, false);
-        test_addic(0xFFFFFFFF'FFFFFFFFULL, 0x0001, 0x00000000'00000000ULL, true);
+        test_addic(0x000000010000FFFFULL, 0x0001, 0x0000000100010000ULL, false);
+        // TODO: test_addic(0x00000000FFFFFFFFULL, 0x0001, 0x0000000100000000ULL, true);
+        // TODO: test_addic(0x00000000FFFF0001ULL, 0xFFFF, 0x00000000FFFF0000ULL, false);
+        test_addic(0xFFFFFFFFFFFFFFFFULL, 0x0001, 0x0000000000000000ULL, true);
     }
 
-    TEST_METHOD(PPU_addzex) {
+    TEST_METHOD(PPC_addzex) {
+    }
+
+    TEST_METHOD(PPC_andx) {
+        // And
+        TEST_INSTRUCTION(test_and_, R1, R2, R3, {
+            state.r[1] = R1;
+            state.r[2] = R2;
+            run(and_(r3, r1, r2));
+            then(state.r[3] == R3);
+            then(!state.cr[0].lt);
+            then(!state.cr[0].gt);
+            then(!state.cr[0].eq);
+            then(!state.cr[0].so);
+            then(!state.xer.so);
+            then(!state.xer.ov);
+            then(!state.xer.ca);
+        });
+
+        test_and_(0x0000111100001111ULL, 0x1111111100000000ULL, 0x0000111100000000ULL);
+        test_and_(0x00000000000000FFULL, 0x000000000000F0F0ULL, 0x00000000000000F0ULL);
+
+        // And (with condition)
+        TEST_INSTRUCTION(test_and__, R1, R2, R3, LT, GT, EQ, SO, {
+            state.r[1] = R1;
+            state.r[2] = R2;
+            run(and__(r3, r1, r2));
+            then(state.r[3] == R3);
+            then(state.cr[0].lt == LT);
+            then(state.cr[0].gt == GT);
+            then(state.cr[0].eq == EQ);
+            then(state.cr[0].so == SO);
+            then(!state.xer.so);
+            then(!state.xer.ov);
+            then(!state.xer.ca);
+        });
+
+        test_and__(0x00000000FFFFFFFFULL, 0xFFFFFFFFFFFFFFFFULL, 0x00000000FFFFFFFFULL, 0,1,0,0);
+        test_and__(0xFFFFFFFF00000000ULL, 0x800000000000FFFFULL, 0x8000000000000000ULL, 1,0,0,0);
+        test_and__(0xFFFFFFFF00000000ULL, 0x00000000FFFFFFFFULL, 0x0000000000000000ULL, 0,0,1,0);
+    }
+
+    TEST_METHOD(PPC_andcx) {
+        // And with complement
+        TEST_INSTRUCTION(test_andc, R1, R2, R3, {
+            state.r[1] = R1;
+            state.r[2] = R2;
+            run(andc(r3, r1, r2));
+            then(state.r[3] == R3);
+            then(!state.cr[0].lt);
+            then(!state.cr[0].gt);
+            then(!state.cr[0].eq);
+            then(!state.cr[0].so);
+            then(!state.xer.so);
+            then(!state.xer.ov);
+            then(!state.xer.ca);
+        });
+
+        test_andc(0x0000111100001111ULL, 0x1111111100000000ULL, 0x0000000000001111ULL);
+        test_andc(0x00000000000000FFULL, 0x000000000000F0F0ULL, 0x000000000000000FULL);
+
+        // And with complement (with condition)
+        TEST_INSTRUCTION(test_andc_, R1, R2, R3, LT, GT, EQ, SO, {
+            state.r[1] = R1;
+            state.r[2] = R2;
+            run(andc_(r3, r1, r2));
+            then(state.r[3] == R3);
+            then(state.cr[0].lt == LT);
+            then(state.cr[0].gt == GT);
+            then(state.cr[0].eq == EQ);
+            then(state.cr[0].so == SO);
+            then(!state.xer.so);
+            then(!state.xer.ov);
+            then(!state.xer.ca);
+        });
+
+        test_andc_(0x00000000FFFFFFFFULL, 0xFFFFFFFFFFFFFFFFULL, 0x0000000000000000ULL, 0,0,1,0);
+        test_andc_(0xFFFFFFFF00005555ULL, 0xFFFFFFFFFFFF0000ULL, 0x0000000000005555ULL, 0,1,0,0);
+        test_andc_(0xFFFFFFFF00000000ULL, 0x00000000FFFFFFFFULL, 0xFFFFFFFF00000000ULL, 1,0,0,0);
+    }
+
+    TEST_METHOD(PPC_andi_) {
+        // Add Immediate
+        TEST_INSTRUCTION(test_andi_, R1, UIMM, R2, LT, GT, EQ, SO, {
+            state.r[1] = R1;
+            run(andi_(r2, r1, UIMM));
+            then(state.r[2] == R2);
+            then(state.cr[0].lt == LT);
+            then(state.cr[0].gt == GT);
+            then(state.cr[0].eq == EQ);
+            then(state.cr[0].so == SO);
+            then(!state.xer.so);
+            then(!state.xer.ov);
+            then(!state.xer.ca);
+        });
+
+        test_andi_(0x0000FFFFFFFFFFF0ULL, 0x000F, 0x0000000000000000ULL, 0,0,1,0);
+        test_andi_(0x0000FFFFFFFFFFFFULL, 0xFFFF, 0x000000000000FFFFULL, 0,1,0,0);
+    }
+
+    TEST_METHOD(PPC_andis_) {
+        // Add Immediate
+        TEST_INSTRUCTION(test_andis_, R1, UIMM, R2, LT, GT, EQ, SO, {
+            state.r[1] = R1;
+            run(andis_(r2, r1, UIMM));
+            then(state.r[2] == R2);
+            then(state.cr[0].lt == LT);
+            then(state.cr[0].gt == GT);
+            then(state.cr[0].eq == EQ);
+            then(state.cr[0].so == SO);
+            then(!state.xer.so);
+            then(!state.xer.ov);
+            then(!state.xer.ca);
+        });
+
+        test_andis_(0x0000FFFFFFF0FFFFULL, 0x000F, 0x0000000000000000ULL, 0,0,1,0);
+        test_andis_(0x0000FFFFFFFFFFFFULL, 0xFFFF, 0x00000000FFFF0000ULL, 0,1,0,0);
     }
 
 #undef run
