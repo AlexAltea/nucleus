24be27060a39f48649e466210e964186c3488688
Alexandro Sánchez Bach <alexandro.sanchez@alumni.uni-heidelberg.de>
2016-02-14 21:25:04 +0100

    Initial X11-based window system for Linux wrapper

--- wrappers/linux/main.cpp
+++ wrappers/linux/main.cpp
@@ -7,9 +7,66 @@
 #include "nucleus/nucleus.h"
 #include "nucleus/core/config.h"
 
-#include <iostream>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+#include <cstring>
 
 int main(int argc, char **argv) {
-    std::cout << "Hello World!" << std::endl;
+    // Load configuration
+    nucleusConfigure(argc, argv);
+
+    // Using UI
+    if (!config.console) {
+        Display* display = XOpenDisplay("");
+        int screen = DefaultScreen(display);
+        unsigned long background = WhitePixel(display, screen);
+        unsigned long foreground = BlackPixel(display, screen);
+
+        XSizeHints hint;
+        hint.x = 200;
+        hint.y = 200;
+        hint.width = 960;
+        hint.height = 544;
+        hint.flags = PSize;
+
+        Window window = XCreateSimpleWindow(display, DefaultRootWindow(display),
+            hint.x, hint.y, hint.width, hint.height, 5, foreground, background);
+
+        const char appName[] = "Nucleus";
+        XSetStandardProperties(display, window, appName, appName, None, argv, argc, &hint);
+
+        GC gc = XCreateGC(display, window, 0, 0);
+        XSetBackground(display, gc, background);
+        XSetForeground(display, gc, foreground);
+        XSelectInput(display, window, ButtonPressMask | KeyPressMask | ExposureMask);
+        XMapRaised(display, window);
+        
+        int done = 0;
+        while (!done) {
+            XEvent evt;
+            XNextEvent(display, &evt);
+            switch(evt.type) {
+            case MappingNotify:
+                XRefreshKeyboardMapping(&evt.xmapping);
+                break;
+            case ButtonPress:
+	        nucleusOnMouseClick(evt.xbutton.x, evt.xbutton.y);
+                break;
+            case KeyPress:
+                break;
+            }
+        }
+
+        XFreeGC(display, gc);
+        XDestroyWindow(display, window);
+        XCloseDisplay(display);
+    }
+
+    // Using console
+    else {
+        nucleusInitialize(argc, argv);
+    }
+    
     return 0;
 }
