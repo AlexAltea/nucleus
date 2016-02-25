95ce10aa6eda6c1c1bed78a7e87212bb75279523
Alexandro Sánchez Bach <alexandro.sanchez@alumni.uni-heidelberg.de>
2016-02-25 22:12:28 +0100

    Added Ant-based build.xml file for Android NDK glue

---
+++ wrappers/android/main.cpp
@@ -0,0 +1,172 @@
+/**
+ * (c) 2014-2016 Alexandro Sanchez Bach. All rights reserved.
+ * Released under GPL v2 license. Read LICENSE for more details.
+ */
+
+#include "nucleus/nucleus.h"
+#include "wrappers/android/android_native_glue.h"
+
+#include <android/sensor.h>
+#include <android/log.h>
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO,  "nucleus-android-native", __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN,  "nucleus-android-native", __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "nucleus-android-native", __VA_ARGS__))
+#define LOGF(...) ((void)__android_log_print(ANDROID_LOG_FATAL, "nucleus-android-native", __VA_ARGS__))
+
+/**
+ * Our saved state data.
+ */
+struct saved_state {
+};
+
+/**
+ * Shared state for our app.
+ */
+struct engine {
+	struct android_app* app;
+
+	ASensorManager* sensorManager;
+	ASensorEventQueue* sensorEventQueue;
+    const ASensor* accelerometerSensor;
+
+	int animating;
+	int32_t width;
+	int32_t height;
+	struct saved_state state;
+};
+
+// Mocks
+static int engine_init_display(struct engine* engine) {
+	return 0;
+}
+static void engine_draw_frame(struct engine* engine) {
+}
+static void engine_term_display(struct engine* engine) {
+}
+
+/**
+* Process the next input event.
+*/
+static int32_t engine_handle_input(struct android_app* app, AInputEvent* event) {
+	struct engine* engine = (struct engine*)app->userData;
+	if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_MOTION) {
+		/*engine->state.x = AMotionEvent_getX(event, 0);
+		engine->state.y = AMotionEvent_getY(event, 0);*/
+		return 1;
+	}
+	return 0;
+}
+
+/**
+* Process the next main command.
+*/
+static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
+	struct engine* engine = (struct engine*)app->userData;
+	switch (cmd) {
+	case APP_CMD_SAVE_STATE:
+		// The system has asked us to save our current state.  Do so.
+		engine->app->savedState = malloc(sizeof(struct saved_state));
+		*((struct saved_state*)engine->app->savedState) = engine->state;
+		engine->app->savedStateSize = sizeof(struct saved_state);
+		break;
+	case APP_CMD_INIT_WINDOW:
+		// The window is being shown, get it ready.
+		if (engine->app->window != NULL) {
+			engine_init_display(engine);
+			engine_draw_frame(engine);
+		}
+		break;
+	case APP_CMD_TERM_WINDOW:
+		// The window is being hidden or closed, clean it up.
+		engine_term_display(engine);
+		break;
+	case APP_CMD_GAINED_FOCUS:
+		// When our app gains focus, we start monitoring the accelerometer.
+		if (engine->accelerometerSensor != NULL) {
+			ASensorEventQueue_enableSensor(engine->sensorEventQueue, engine->accelerometerSensor);
+			// We'd like to get 60 events per second (in us).
+			ASensorEventQueue_setEventRate(engine->sensorEventQueue, engine->accelerometerSensor, (1000L / 60) * 1000);
+		}
+		break;
+	case APP_CMD_LOST_FOCUS:
+		// When our app loses focus, we stop monitoring the accelerometer.
+		// This is to avoid consuming battery while not being used.
+		if (engine->accelerometerSensor != NULL) {
+			ASensorEventQueue_disableSensor(engine->sensorEventQueue, engine->accelerometerSensor);
+		}
+		// Also stop animating.
+		engine->animating = 0;
+		engine_draw_frame(engine);
+		break;
+	}
+}
+
+/**
+* This is the main entry point of a native application that is using
+* android_native_glue.  It runs in its own thread, with its own
+* event loop for receiving input events and doing other things.
+*/
+void android_main(struct android_app* state) {
+	struct engine engine;
+
+	memset(&engine, 0, sizeof(engine));
+	state->userData = &engine;
+	state->onAppCmd = engine_handle_cmd;
+	state->onInputEvent = engine_handle_input;
+	engine.app = state;
+
+	// Prepare to monitor accelerometer
+	engine.sensorManager = ASensorManager_getInstance();
+	engine.accelerometerSensor = ASensorManager_getDefaultSensor(engine.sensorManager,
+		ASENSOR_TYPE_ACCELEROMETER);
+	engine.sensorEventQueue = ASensorManager_createEventQueue(engine.sensorManager,
+		state->looper, LOOPER_ID_USER, NULL, NULL);
+
+	if (state->savedState != NULL) {
+		// We are starting with a previous saved state; restore from it.
+		engine.state = *(struct saved_state*)state->savedState;
+	}
+
+	engine.animating = 1;
+
+	// loop waiting for stuff to do.
+
+	while (1) {
+		// Read all pending events.
+		int ident;
+		int events;
+		struct android_poll_source* source;
+
+		// If not animating, we will block forever waiting for events.
+		// If animating, we loop until all events are read, then continue
+		// to draw the next frame of animation.
+		while ((ident = ALooper_pollAll(engine.animating ? 0 : -1, NULL, &events, (void**)&source)) >= 0) {
+
+			// Process this event.
+			if (source != NULL) {
+				source->process(state, source);
+			}
+
+			// If a sensor has data, process it now.
+			if (ident == LOOPER_ID_USER) {
+				if (engine.accelerometerSensor != NULL) {
+					ASensorEvent event;
+					while (ASensorEventQueue_getEvents(engine.sensorEventQueue, &event, 1) > 0) {
+						LOGI("accelerometer: x=%f y=%f z=%f", event.acceleration.x, event.acceleration.y, event.acceleration.z);
+					}
+				}
+			}
+
+			// Check if we are exiting.
+			if (state->destroyRequested != 0) {
+				engine_term_display(&engine);
+				return;
+			}
+		}
+
+		if (engine.animating) {
+			engine_draw_frame(&engine);
+		}
+	}
+}
