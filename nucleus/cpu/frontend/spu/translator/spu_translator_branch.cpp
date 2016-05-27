26216a0f328c03fd4b97e9155b7108915d6be899
Alexandro Sánchez Bach <alexandro.sanchez@alumni.uni-heidelberg.de>
2016-05-27 14:40:28 +0200

    Implemented branching/comparison SPU instructions

--- nucleus/cpu/frontend/spu/translator/spu_translator_branch.cpp
+++ nucleus/cpu/frontend/spu/translator/spu_translator_branch.cpp
@@ -6,6 +6,18 @@
 #include "spu_translator.h"
 #include "nucleus/core/config.h"
 #include "nucleus/assert.h"
+#include "nucleus/cpu/frontend/spu/spu_state.h"
+#include "nucleus/cpu/frontend/spu/spu_thread.h"
+
+#define INTERPRET(func) \
+    builder.createCall(builder.getExternFunction( \
+        reinterpret_cast<void*>( \
+        reinterpret_cast<uintptr_t>( \
+        static_cast<void(*)(Instruction)>([](Instruction o) { \
+            auto& state = *static_cast<frontend::spu::SPUThread*>(CPU::getCurrentThread())->state.get(); \
+            func \
+        }))), \
+    TYPE_VOID, { TYPE_I32 }), { builder.getConstantI32(code.value) });
 
 namespace cpu {
 namespace frontend {
@@ -22,7 +34,7 @@ using namespace cpu::hir;
 // Compare, Branch and Halt Instructions (Chapter 7)
 void Translator::bi(Instruction code)
 {
-    assert_always("Unimplemented");
+    builder.createRet();
 }
 
 void Translator::bihnz(Instruction code)
@@ -72,17 +84,38 @@ void Translator::brasl(Instruction code)
 
 void Translator::brhnz(Instruction code)
 {
-    assert_always("Unimplemented");
+    const U32 targetAddr = currentAddress + (code.i16 << 2);
+    const U32 nextAddr = currentAddress + 4;
+
+    Value* ps = builder.createExtract(getGPR(code.rt), builder.getConstantI8(7), TYPE_I16);
+    Value* zero = builder.getConstantI16(0);
+    Value* cond = builder.createCmpNE(ps, zero);
+
+    builder.createBrCond(cond, blocks.at(targetAddr), blocks.at(nextAddr));
 }
 
 void Translator::brhz(Instruction code)
 {
-    assert_always("Unimplemented");
+    const U32 targetAddr = currentAddress + (code.i16 << 2);
+    const U32 nextAddr = currentAddress + 4;
+
+    Value* ps = builder.createExtract(getGPR(code.rt), builder.getConstantI8(7), TYPE_I16);
+    Value* zero = builder.getConstantI16(0);
+    Value* cond = builder.createCmpEQ(ps, zero);
+
+    builder.createBrCond(cond, blocks.at(targetAddr), blocks.at(nextAddr));
 }
 
 void Translator::brnz(Instruction code)
 {
-    assert_always("Unimplemented");
+    const U32 targetAddr = currentAddress + (code.i16 << 2);
+    const U32 nextAddr = currentAddress + 4;
+
+    Value* ps = builder.createExtract(getGPR(code.rt), builder.getConstantI8(3), TYPE_I32);
+    Value* zero = builder.getConstantI32(0);
+    Value* cond = builder.createCmpNE(ps, zero);
+
+    builder.createBrCond(cond, blocks.at(targetAddr), blocks.at(nextAddr));
 }
 
 void Translator::brsl(Instruction code)
@@ -102,97 +135,203 @@ void Translator::brsl(Instruction code)
 
 void Translator::brz(Instruction code)
 {
-    assert_always("Unimplemented");
+    const U32 targetAddr = currentAddress + (code.i16 << 2);
+    const U32 nextAddr = currentAddress + 4;
+
+    Value* ps = builder.createExtract(getGPR(code.rt), builder.getConstantI8(3), TYPE_I32);
+    Value* zero = builder.getConstantI32(0);
+    Value* cond = builder.createCmpEQ(ps, zero);
+
+    builder.createBrCond(cond, blocks.at(targetAddr), blocks.at(nextAddr));
 }
 
 void Translator::ceq(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        const auto& rb = state.r[o.rb];
+        for (Size i = 0; i < 4; i++) {
+            state.r[o.rt].u32[i] = (ra.s32[i] == rb.s32[i]) ? 0xFFFFFFFF : 0;
+        }
+    });
 }
 
 void Translator::ceqb(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        const auto& rb = state.r[o.rb];
+        for (Size i = 0; i < 16; i++) {
+            state.r[o.rt].u8[i] = (ra.u8[i] == rb.u8[i]) ? 0xFF : 0;
+        }
+    });
 }
 
 void Translator::ceqbi(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        for (Size i = 0; i < 16; i++) {
+            state.r[o.rt].s8[i] = (ra.s8[i] == (S08)(o.i10 & 0xFF)) ? 0xFF : 0;
+        }
+    });
 }
 
 void Translator::ceqh(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        const auto& rb = state.r[o.rb];
+        for (Size i = 0; i < 8; i++) {
+            state.r[o.rt].u16[i] = (ra.u16[i] == rb.u16[i]) ? 0xFFFF : 0;
+        }
+    });
 }
 
 void Translator::ceqhi(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        for (Size i = 0; i < 8; i++)
+            state.r[o.rt].u16[i] = (ra.s16[i] == (S16)(o.i10)) ? 0xFFFF : 0;
+    
+    });
 }
 
 void Translator::ceqi(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        for (Size i = 0; i < 4; i++) {
+            state.r[o.rt].u32[i] = (ra.s32[i] == (S32)(o.i10)) ? 0xFFFFFFFF : 0;
+        }
+    });
 }
 
 void Translator::cgt(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        const auto& rb = state.r[o.rb];
+        for (Size i = 0; i < 4; i++) {
+            state.r[o.rt].u32[i] = (ra.s32[i] > rb.s32[i]) ? 0xFFFFFFFF : 0;
+        }
+    });
 }
 
 void Translator::cgtb(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        const auto& rb = state.r[o.rb];
+        for (Size i = 0; i < 16; i++) {
+            state.r[o.rt].u8[i] = (ra.s8[i] > rb.s8[i]) ? 0xFF : 0;
+        }
+    });
 }
 
 void Translator::cgtbi(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        for (Size i = 0; i < 16; i++) {
+            state.r[o.rt].u8[i] = (ra.s8[i] > (S08)(o.i10 & 0xFF)) ? 0xFF : 0;
+        }
+    });
 }
 
 void Translator::cgth(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        const auto& rb = state.r[o.rb];
+        for (Size i = 0; i < 8; i++) {
+            state.r[o.rt].u16[i] = (ra.s16[i] > rb.s16[i]) ? 0xFFFF : 0;
+        }
+    });
 }
 
 void Translator::cgthi(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        for (Size i = 0; i < 8; i++) {
+            state.r[o.rt].u16[i] = (ra.s16[i] > (S16)(o.i10)) ? 0xFFFF : 0;
+        }
+    });
 }
 
 void Translator::cgti(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        for (Size i = 0; i < 4; i++) {
+            state.r[o.rt].u32[i] = (ra.s32[i] > (S32)(o.i10)) ? 0xFFFFFFFF : 0;
+        }
+    });
 }
 
 void Translator::clgt(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        const auto& rb = state.r[o.rb];
+        for (Size i = 0; i < 4; i++) {
+            state.r[o.rt].u32[i] = (ra.u32[i] > rb.u32[i]) ? 0xFFFFFFFF : 0;
+        }
+    });
 }
 
 void Translator::clgtb(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        const auto& rb = state.r[o.rb];
+        for (Size i = 0; i < 16; i++) {
+            state.r[o.rt].u8[i] = (ra.u8[i] > rb.u8[i]) ? 0xFF : 0;
+        }
+    });
 }
 
 void Translator::clgtbi(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        for (Size i = 0; i < 16; i++) {
+            state.r[o.rt].u8[i] = (ra.u8[i] > (U08)(o.i10 & 0xFF)) ? 0xFF : 0;
+        }
+    });
 }
 
 void Translator::clgth(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        const auto& rb = state.r[o.rb];
+        for (Size i = 0; i < 8; i++) {
+            state.r[o.rt].u16[i] = (ra.u16[i] > rb.u16[i]) ? 0xFFFF : 0;
+        }
+    });
 }
 
 void Translator::clgthi(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        for (Size i = 0; i < 8; i++) {
+			state.r[o.rt].u16[i] = (ra.u16[i] > (U16)(o.i10)) ? 0xFFFF : 0;
+		}
+    });
 }
 
 void Translator::clgti(Instruction code)
 {
-    assert_always("Unimplemented");
+    INTERPRET({
+        const auto& ra = state.r[o.ra];
+        for (Size i = 0; i < 4; i++) {
+            state.r[o.rt].u32[i] = (ra.u32[i] > (U32)(o.i10)) ? 0xFFFFFFFF : 0;
+        }
+    });
 }
 
 void Translator::heq(Instruction code)
